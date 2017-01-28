a61837e10c0393469ac49738b0ac02c3575e279e
Micah Hauge <micahhauge@gmail.com>
2017-01-28 10:23:05 -0600

    Exons now position themselves horizontaly using OOP approach.

--- WebApp/js/gfv.js
+++ WebApp/js/gfv.js
@@ -21,8 +21,7 @@ p.center = p.height / 2.5;
 class Graph {
     constructor() {
         // setup blank visual with proper dimensions
-        this.svg = d3.select("body").append("svg").attr("width", 500).attr("height", 500);
-        console.log(this.svg);
+        this.svg = d3.select("body").append("svg").attr("width", p.width).attr("height", p.height);
         this.edgesLayer = this.svg.append('g');
         this.exonLayer = this.svg.append('g');
 
@@ -31,18 +30,17 @@ class Graph {
         this.exons = [];
 
         // 2D array of exons at each x position
-        this.exonsColumn = new Array(50);
+        this.exonsInColumn = new Array(50);
         for (var i = 0; i < 20; i++) {
-          this.exonsColumn[i] = new Array();
+          this.exonsInColumn[i] = new Array();
         }
     }
 
-    addExon(collumn) {
-        console.log("adding exon");
-        console.log(this.svg);
-        var exon = new Exon(collumn);
+    addExon(column) {
+        // create and exon
+        var exon = new Exon(column);
 
-
+        // create a graphic for the exon
         var graphic = this.svg.append("ellipse");
         graphic.attr("cx", exon.x)
                .attr("cy", exon.y)
@@ -53,22 +51,43 @@ class Graph {
                .style("fill", "white");
 
 
-        // make graphic grow
+        // make graphic grow on hover
         graphic.on('mouseover', function(d){
-            console.log("ENTER");
             d3.select(this).style("fill", "lightblue")
             d3.select(this).transition().attr("ry", exon.growRadius)
                                         .attr("rx", exon.growRadius)
                                         .duration(300);
         })
-        // make graphic shrinjk
+        // make graphic shrink on exit
         graphic.on('mouseout', function(d){
-            console.log("Exit");
             d3.select(this).style("fill", "white")
             d3.select(this).transition().attr("ry", exon.radius)
                                         .attr("rx", exon.radius)
                                         .duration(300);
         })
+
+        // add the graphic to the exon
+        exon.graphic = graphic;
+
+        // add exon to exon list
+        this.exons.push(exon);
+
+        // add exon to exons in column list
+        this.exonsInColumn[column].push(exon);
+
+
+        // if there are more than one exons in a column, adjust positions of exons
+        if (this.exonsInColumn[column].length > 1) {
+            var totalHeight = p.yPadding * (this.exonsInColumn[column].length-1);
+            var topExonPos = p.center - (totalHeight * .5);
+
+            // reposition all of the exons at proper positions
+            for (var i = 0; i < this.exonsInColumn[column].length; i++) {
+                this.exonsInColumn[column][i].graphic.attr("cy", topExonPos + (p.yPadding * i));
+                console.log(this.exonsInColumn[column][i].graphic.attr("cy"));
+            }
+        }
+
     }
 }
 
@@ -94,7 +113,7 @@ class Exon {
         this.outEdges = [];
 
         // exon graphic
-        this.exonGraphic = null;
+        this.graphic = null;
     }
 }
 
@@ -102,6 +121,7 @@ class Exon {
 // main
 var visual = new Graph();
 visual.addExon(2);
+visual.addExon(2);
+visual.addExon(2);
+visual.addExon(2);
 // var exon = new Exon(1);
-// console.log(p);
-// console.log(exon);
